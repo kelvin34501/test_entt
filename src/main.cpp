7abc2810659acdd97dd283d9e48ff1f54b376edb
kelvin34501 <kelvin34501@gmail.com>
2020-09-20 11:41:05 +0800

    test tbb std::execution

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <exception>
 #include <utility>
 #include <execution>
+#include <random>
+#include <chrono>
 
 #include <entt/entt.hpp>
 
@@ -34,23 +36,23 @@ void update(entt::registry &registry) {
 }
 
 void update(std::uint64_t dt, entt::registry &registry) {
-    registry.view<Position, Velocity>().each(
-            [dt](auto &pos, auto &vel) {
-                // gets all the components of the view at once
-                pos.x += vel.dx * dt;
-                pos.y += vel.dy * dt;
-            }
-    );
-//    auto view = registry.view<Position, Velocity>();
-//    std::for_each(
-//            std::execution::par, view.begin(), view.end(),
-//            [&view, dt](const auto entity) {
-//                auto &pos = view.get<Position>(entity);
-//                const auto &vel = view.get<Velocity>(entity);
+//    registry.view<Position, Velocity>().each(
+//            [dt](auto &pos, auto &vel) {
+//                // gets all the components of the view at once
 //                pos.x += vel.dx * dt;
 //                pos.y += vel.dy * dt;
 //            }
 //    );
+    auto view = registry.view<Position, Velocity>();
+    std::for_each(
+            std::execution::par_unseq, view.begin(), view.end(),
+            [&view, dt](const auto entity) {
+                auto &pos = view.get<Position>(entity);
+                const auto &vel = view.get<Velocity>(entity);
+                pos.x += vel.dx * dt;
+                pos.y += vel.dy * dt;
+            }
+    );
 }
 
 void show_position(entt::registry &registry) {
@@ -67,22 +69,68 @@ int main(int argn, char *argv[]) {
     entt::registry registry;
     std::uint64_t dt = 16;
 
-    for (auto i = 0; i < 100000000; ++i) {
-        auto entity = registry.create();
-        registry.emplace<Position>(entity, i * 1.0f, i * 1.0f);
-        if (i % 2 == 0) {
-            registry.emplace<Velocity>(
-                    entity, i * 0.1f, i * 0.1f
-            );
-        }
-    }
+//    for (auto i = 0; i < 100000000; ++i) {
+//        auto entity = registry.create();
+//        registry.emplace<Position>(entity, i * 1.0f, i * 1.0f);
+//        if (i % 2 == 0) {
+//            registry.emplace<Velocity>(
+//                    entity, i * 0.1f, i * 0.1f
+//            );
+//        }
+//    }
 
 //    show_position(registry);
-    std::cout << "=== split ===\n";
-    for (auto i = 0; i < 32; ++i) {
-        update(dt, registry);
-    }
-    update(registry);
+//    std::cout << "=== split ===\n";
+//    for (auto i = 0; i < 32; ++i) {
+//        update(dt, registry);
+//    }
+//    update(registry);
 //    show_position(registry);
+
+    std::random_device rnd_dev;
+    std::mt19937 mersenne_engine{rnd_dev()};
+    std::uniform_int_distribution<int32_t> dist{1, 60};
+    auto gen = [&dist, &mersenne_engine] {
+        return dist(mersenne_engine);
+    };
+    std::vector<int> vec(100000000);
+    auto print_container_simple_type = [](const auto &container) {
+        for (const auto elem: container) {
+            std::cout << elem <<", ";
+        }
+        std::cout << '\n';
+    };
+//    print_container_simple_type(vec);
+
+    std::generate(begin(vec), end(vec), gen);
+    const auto start_1 = std::chrono::steady_clock::now();
+    std::sort(std::execution::seq, begin(vec), end(vec));
+    const auto end_1 = std::chrono::steady_clock::now();
+    auto diff_1 = end_1 - start_1;
+    std::cout << std::chrono::duration<double, std::milli>(diff_1).count() << " ms\n";
+
+    std::generate(begin(vec), end(vec), gen);
+    const auto start_2 = std::chrono::steady_clock::now();
+    std::sort(std::execution::par, begin(vec), end(vec));
+    const auto end_2 = std::chrono::steady_clock::now();
+    auto diff_2 = end_2 - start_2;
+    std::cout << std::chrono::duration<double, std::milli>(diff_2).count() << " ms\n";
+
+    std::generate(begin(vec), end(vec), gen);
+    const auto start_3 = std::chrono::steady_clock::now();
+    std::sort(std::execution::par_unseq, begin(vec), end(vec));
+    const auto end_3 = std::chrono::steady_clock::now();
+    auto diff_3 = end_3 - start_3;
+    std::cout << std::chrono::duration<double, std::milli>(diff_3).count() << " ms\n";
+
+    std::generate(begin(vec), end(vec), gen);
+    const auto start_4 = std::chrono::steady_clock::now();
+    std::sort(begin(vec), end(vec));
+    const auto end_4 = std::chrono::steady_clock::now();
+    auto diff_4 = end_4 - start_4;
+    std::cout << std::chrono::duration<double, std::milli>(diff_4).count() << " ms\n";
+
+//    print_container_simple_type(vec);
+
     return 0;
 }
